e16422552c0415b479b7e42b5da55a91d988488c
MasterTinka <znamenochick@gmail.com>
2021-03-30 22:08:37 +0600

    Добавлено управление телевизором

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,10 @@
 #include "digitalPinClassOutput.h"
 #include "IRremote.h"
 
-
 digitalPinClassOutput relay(5);
 digitalPinClassInput button(6);
 IRrecv Reciever(A0);
+IRsend Sender;
 
 void setup() 
 {
@@ -20,6 +20,7 @@ void loop() {
       Reciever.decode();
       delay(10);
       auto IRvalue = Reciever.results.value & 0xFF;
+      Serial.println(Reciever.results.value, HEX);
       if (IRvalue == 0xC)
       {
           relay.toggle_Status();
@@ -45,9 +46,50 @@ void loop() {
       {
         Serial.print(5);
         delay(200);
-        }
-      
+      }
 
+      else if(IRvalue == 0x38)
+      {
+        Sender.sendLG(0x20DF10EF, 32);
+        delay(200);
+      }
+      else if(IRvalue == 0xE)
+      {
+        Sender.sendLG(0x20DFD02F, 32);
+        delay(200);
+      }
+      else if(IRvalue == 0x2)
+      {
+        Sender.sendLG(0x20DF02FD, 32);
+        delay(200);
+      }
+      else if(IRvalue == 0x5)
+      {
+        Sender.sendLG(0x20DFE01F, 32);
+        delay(200);
+      }
+      else if(IRvalue == 0x7)
+      {
+        Sender.sendLG(0x20DF609F, 32);
+        delay(200);
+      }
+      else if(IRvalue == 0x0)
+      {
+        Sender.sendLG(0x20DF827D, 32);
+        delay(200);
+      }
+      else if(IRvalue == 0x6)
+      {
+        Sender.sendLG(0x20DF22DD, 32);
+        delay(200);
+      }
+      else if(IRvalue == 0x3F)
+      {
+        Sender.sendLG(0x20DFDA25, 32);
+        delay(200);
+      }
+      
+      Reciever.enableIRIn();
       Reciever.resume();
   }
 
