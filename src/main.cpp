9fbb13be7aca5c2ca65148dd50ef030bf52900a4
MasterTinka <znamenochick@gmail.com>
2021-03-31 19:34:32 +0600

    Добавлен функционал управления звуком и включения с кнопки

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,11 @@
 digitalPinClassOutput relay(5);
 digitalPinClassInput button(6);
 IRrecv Reciever(A0);
-IRsend Sender;
+IRsend Sender;                // 3 Контакт
 
 void setup() 
 {
+    relay.set_HIGH();
     Reciever.enableIRIn();
     Serial.begin(115200);
 }
@@ -19,9 +20,9 @@ void loop() {
   {
       Reciever.decode();
       delay(10);
-      auto IRvalue = Reciever.results.value & 0xFF;
-      Serial.println(Reciever.results.value, HEX);
-      if (IRvalue == 0xC)
+      auto IRvalue = Reciever.results.value & 0xFF; // Снятие значения по маске
+      //Serial.println(Reciever.results.value, HEX);
+      if (IRvalue == 0xC) // Включение реле
       {
           relay.toggle_Status();
           delay(1000);
@@ -47,45 +48,56 @@ void loop() {
         Serial.print(5);
         delay(200);
       }
-
+      
+      // ТЕЛЕВИЗОР
       else if(IRvalue == 0x38)
       {
-        Sender.sendLG(0x20DF10EF, 32);
+        Sender.sendLG(0x20DF10EF, 32); // Включение
         delay(200);
       }
       else if(IRvalue == 0xE)
       {
-        Sender.sendLG(0x20DFD02F, 32);
+        Sender.sendLG(0x20DFD02F, 32); // Открытие меню
         delay(200);
       }
       else if(IRvalue == 0x2)
       {
-        Sender.sendLG(0x20DF02FD, 32);
+        Sender.sendLG(0x20DF02FD, 32); // Стрелка вверх
         delay(200);
       }
       else if(IRvalue == 0x5)
       {
-        Sender.sendLG(0x20DFE01F, 32);
+        Sender.sendLG(0x20DFE01F, 32); // Стрелка влево
         delay(200);
       }
-      else if(IRvalue == 0x7)
+      else if(IRvalue == 0x7)          
       {
-        Sender.sendLG(0x20DF609F, 32);
+        Sender.sendLG(0x20DF609F, 32); // Стрелка вправо
         delay(200);
       }
       else if(IRvalue == 0x0)
       {
-        Sender.sendLG(0x20DF827D, 32);
+        Sender.sendLG(0x20DF827D, 32);  // Стелка вниз
         delay(200);
       }
       else if(IRvalue == 0x6)
       {
-        Sender.sendLG(0x20DF22DD, 32);
+        Sender.sendLG(0x20DF22DD, 32);  // Выбор
         delay(200);
       }
       else if(IRvalue == 0x3F)
       {
-        Sender.sendLG(0x20DFDA25, 32);
+        Sender.sendLG(0x20DFDA25, 32);  // Выход
+        delay(200);
+      }
+      else if(IRvalue == 0x4)
+      {
+        Sender.sendLG(0x20DF40BF, 32);  // Увеличить звук
+        delay(200);
+      }
+      else if(IRvalue == 0x8)
+      {
+        Sender.sendLG(0x20DFC03F, 32);  // Уменьшить звук
         delay(200);
       }
       
@@ -93,5 +105,9 @@ void loop() {
       Reciever.resume();
   }
 
-  
+  if(button.read_data() == true)
+  {
+    relay.toggle_Status();
+    delay(500);
+  }
 }
